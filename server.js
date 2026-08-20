9e833372bc1a52a291e9a504498f32b964ee0f39
Anchit_Agrawal <anchitagrawal246@gmail.com>
2026-08-20 11:46:51 +0530

    mongo db successfully connected

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
-const connectTodb = require("./src/db/database")
+const connectTodb = require("./src/config/database");
 require("dotenv").config();
-const port = process.env.PORT
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`"THIS SERVER IS STARTED AT PORT ${port} "`);
 });
--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const mongoUri = process.env.MONGO_URI;
 async function ConnectToDb() {
